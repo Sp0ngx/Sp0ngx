03b1d2447b5590c133bb8691e0ca34e532206358
Nicholas <nicholaskow21025@gmail.com>
2025-05-24 17:54:35 +0800

    Update PokemonContext.cs

--- PokemonContext.cs
+++ PokemonContext.cs
@@ -5,10 +5,60 @@ namespace PokemonPocket
     public class PokemonContext : DbContext
     {
         public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<PokeDex> PokeDex { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=pokemonPocket.db");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<PokeDex>().HasData(
+        new PokeDex
+        {
+            Id = 1,
+            Name = "Pikachu",
+            Skill = "Lightning Bolt",
+            SkillDmg = 30
+        },
+        new PokeDex
+        {
+            Id = 2,
+            Name = "Eevee",
+            Skill = "Run Away",
+            SkillDmg = 25
+        },
+        new PokeDex
+        {
+            Id = 3,
+            Name = "Charmander",
+            Skill = "Solar Power",
+            SkillDmg = 10
+        },
+        new PokeDex
+        {
+            Id = 4,
+            Name = "Raichu",
+            Skill = "Lightning Bolt",
+            SkillDmg = 30
+        },
+        new PokeDex
+        {
+            Id = 5,
+            Name = "Flareon",
+            Skill = "Run Away",
+            SkillDmg = 25
+        },
+        new PokeDex
+        {
+            Id = 6,
+            Name = "Charmeleon",
+            Skill = "Solar Power",
+            SkillDmg = 10
+        }
+    );
+}
+
     }
 }
